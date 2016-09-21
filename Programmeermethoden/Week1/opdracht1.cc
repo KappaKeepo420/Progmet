9ddb6947bcd9bed8380b169b483a5c07e4604e9e
Lennard Schaap <lennard.schaap@hotmail.com>
2016-09-21 14:37:05 +0200

    test

--- Programmeermethoden/Week1/opdracht1.cc
+++ Programmeermethoden/Week1/opdracht1.cc
@@ -22,8 +22,12 @@ bool geldige_datum(int d, int m, int j) {
 
     bool schrikkel = schrikkeljaar(j);
 
-    if (d < 1) {
+    if ((d < 1) || (m < 1)) {
         return false;
+    }
+
+    if ((d > 31) || (m > 12)) {
+    	return false;
     }
 
     if (j < 1900) {
@@ -50,18 +54,24 @@ bool geldige_datum(int d, int m, int j) {
     return true;
 }
 
-int main ( ) {
-	
-	int j, m, d;
-	int jaarnu, maandnu, dagnu;
-	int leeftijdj;
-	int leeftijdm;
-	int aantaljaren;
-	int aantalmaanden;
-	int maandextra;
-	string jeu;
+int bereken_dag(int d, int m, int j) {
 
-	string dagen [7] = {"Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"};
+	int a = j - (14 - m) / 12;
+	int b = a + a / 4 - a / 100 + a / 400;
+	int c = m + 12 * ((14 - m) / 12) - 2;
+	int dag = (d + b + (31 * c) / 12) % 7;
+
+	return dag;
+}
+
+int bereken_leeftijd(int d, int m, int j) {
+
+	int jaarnu, maandnu, dagnu;
+	int leeftijdm, leeftijdj;
+	int aantaljaren, aantalmaanden;
+	int maandextra;
+	string jeuw = "";
+	string jeu = "";
 
 	tm s;
 	time_t t;
@@ -73,62 +83,120 @@ int main ( ) {
 	maandnu = s.tm_mon + 1;
 	dagnu	= s.tm_mday;
 
-	cout << "Geef uw geboortedatum (dd/mm/jjjj): ";
-
-   	cin >> d;
-
-	if (cin.get() != '/') {
-		cout << "Onjuiste input, probeer opnieuw.\n";
-		return 1;
-	}
-
-	cin >> m;
-
-	if (cin.get() != '/') {
-		cout << "Onjuiste input, probeer opnieuw.\n";
-		return 1;
-	}
-
-	cin >> j;
-
-	
-
-	cout << "Leeftijd in jaren: " << leeftijdj << " jaren en " << maandextra << " maanden.\n";
-	cout << "Leeftijd in maanden: " << leeftijdm << "\n";
-
-	int a = j - (14 - m) / 12;
-	int b = a + a / 4 - a / 100 + a / 400;
-	int c = m + 12 * ((14 - m) / 12) - 2;
-	int dag = (d + b + (31 * c) / 12) % 7;
-
-	cout << "U bent geboren op een: " + dagen[dag-1] << endl;
-
-	return 1;
-}
-
-tuple<int, int, int> bereken_leeftijd(int d, int m, int j) {
-	
 	aantaljaren = jaarnu - j;
 
 	if (m > maandnu) {
 		leeftijdj = aantaljaren - 1;
-	}
-	else {
+	} else {
 		leeftijdj = aantaljaren;
 	}
 
-	cout << leeftijdj << "\n";
-
 	if (m >= maandnu) {
 		maandextra = m - maandnu;
-	}
-	else {
-		maandextra = 12 - maandnu + m;
+	} else {
+		maandextra = 12 - maandnu;
 	}
 
 	aantalmaanden = leeftijdj * 12 + maandextra;
 
 	if (d > dagnu) {
 		leeftijdm = aantalmaanden - 1;
+	} else {
+		leeftijdm = aantalmaanden;
 	}
+
+	if (leeftijdj > 100) {
+		cout << "U bent geschikt \u2610  ongeschikt \u2611 .\n";
+		return NULL;
+	}
+	if (leeftijdj < 10) {
+		cout << "Je bent geschikt \u2610  ongeschikt \u2611 .\n";
+		return NULL;
+	}
+
+	if (leeftijdj > 30) {
+		jeuw = "Uw";
+	} else {
+		jeuw = "Jouw";
+	}
+
+	cout << jeuw << " leeftijd in jaren: " << leeftijdj << " jaar en " << maandextra
+				 << " maanden.\n";
+	cout << jeuw << " leeftijd in maanden: " << leeftijdm << ".\n";
+
+
+	if (d == dagnu && m == maandnu) {
+		cout << "Gefeliciteerd met " << jeuw << " verjaardag!";
+	}
+
+	else if (d == dagnu) {
+		cout << "Gefeliciteerd met " << jeuw << " vermaandag!";
+
+	}
+
+	return leeftijdj;
+}
+
+int main () {
+	
+	int d, m, j;
+	int juisteantwoord;
+	string antwoord;
+
+	tm s;
+	time_t t;
+	time (&t);
+	s = * localtime (&t);
+	int jaarnu  = s.tm_year + 1900;
+
+	string dagen [7] = {"Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag",
+						"Zaterdag", "Zondag"};
+
+	string dagenafk [7] = {"m", "di", "w", "do", "v"
+						, "za", "zo"};
+
+	cout << "Geef uw geboortejaar: ";
+	cin >> j;
+
+	if (j < jaarnu - 102) {
+		cout << "U bent geschikt \u2610  ongeschikt \u2611 .\n";
+		return 1;
+	}
+	if (j > jaarnu - 10) {
+		cout << "Je bent geschikt \u2610  ongeschikt \u2611 .\n";
+		return 1;
+	}
+
+	cout << "Geef uw geboortemaand: ";
+	cin >> m;
+
+	cout << "Geef uw geboortedag: ";
+ 	cin >> d;
+
+	if (geldige_datum(d, m, j)) {
+		if (bereken_leeftijd(d, m, j) == NULL) {
+			return 1;
+		}
+	} else {
+		cout << "Voer een geldige datum in.\n";
+	}
+
+	cout << "Op welke dag van de week bent u geboren? ";
+	cin >> antwoord;
+
+	juisteantwoord = bereken_dag(d, m, j);
+
+	for (int i = 0; i < sizeof(dagenafk); i++) {
+		if (dagenafk[i] == antwoord) {
+			if (i == juisteantwoord) {
+				cout << "Correct!";
+			} else {
+				cout << "Helaas, het juiste antwoord is: " << dagen[juisteantwoord];
+			}
+		} else {
+			cout << "Geef uw dag met 1 of 2 letters.";
+		}
+	}
+
+	return 1;
 }
