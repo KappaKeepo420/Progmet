0cacf8f6338d94e6a39b76ea457807633760b126
jortseljongen <jort_gijzen@outlook.com>
2017-02-23 16:18:17 +0100

    ljhadsflkasfdlkh

--- Programmeertechnieken/hw1/lib/mylib.c
+++ Programmeertechnieken/hw1/lib/mylib.c
@@ -1,3 +1,14 @@
+/*  Auteurs         - Jort Gijzen       1874233
+                    - Lennard Schaap    1914839
+    Studie          - BSc Informatica
+    Datum           - 16 februari 2017
+    Opdracht        - Building and Linking a library.
+    File						- Makefile
+
+    Dit is de mylib.c file. Hierin staan functies die worden aangeroepen in
+    de test file om te kijken of deze twee aan elkaar correct gelinked zijn.
+*/
+
 #include "mylib.h"
 
 int telop (int x, int y) {
--- Programmeertechnieken/hw1/lib/mylib.h
+++ Programmeertechnieken/hw1/lib/mylib.h
@@ -1,3 +1,14 @@
+/*  Auteurs         - Jort Gijzen       1874233
+                    - Lennard Schaap    1914839
+    Studie          - BSc Informatica
+    Datum           - 16 februari 2017
+    Opdracht        - Building and Linking a library.
+    File						- Makefile
+
+    Dit is de header file van de library folder. Hierin staan alle functies
+    die worden gebruikt in deze opdracht.
+*/
+
 #ifndef MYLIB_H
 #define MYLIB_H
 
